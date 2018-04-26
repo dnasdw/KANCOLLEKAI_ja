d45ba13060758e721a87971640d821631aee491c
Sun Daowen <sundaowen@gmail.com>
2018-04-26 13:48:55 +0800

    add script

---
+++ 1_split.bat
@@ -0,0 +1,4 @@
+PUSHD "%~dp0"
+tools\filetool -svfd PCSG00684.VPK PCSG00684.dir
+tools\filetool -svfd PCSG00684_patch.VPK PCSG00684_patch.dir
+POPD
---
+++ 2_join.bat
@@ -0,0 +1,4 @@
+PUSHD "%~dp0"
+tools\filetool -jvfd PCSG00684.VPK PCSG00684.dir
+tools\filetool -jvfd PCSG00684_patch.VPK PCSG00684_patch.dir
+POPD
